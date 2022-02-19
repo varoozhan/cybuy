9aab7444c06dd64a1cdb58a2ccb35aa2aa40a99c
varoozhan <varoozhan@gmail.com>
2022-02-19 11:59:28 -0800

    Added backend outline

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cybuy
+
 Cybersecurity for all
+
+# Questions to ask the user:
+
+- Do you work from home using computers? yes/no
+- Is your wifi password the default that came with the router?
+- Do you do distributed programming?
+- Do you use a public network too connect the device to the internet?
+- Are you a IoT nerd?
---
+++ server/src/main.py
@@ -0,0 +1,28 @@
+
+def run_cybuy(vendor, product, user_input):
+
+    # Search NVD using vendor and product
+    nvd_entries = search_db(vendor , product)
+    # Extract relevant infor from the entries that were returned by search_db
+    relevant_info = relevant_extractor(nvd_results)
+    # Algorithm to calculate the score
+    score = risk_calculator(relevant_info, user_input)
+    # Store the user input into the database
+    analysis_info = db_store(user_input, score)
+    # A general analysis of the database like returning a summary statistics or smth.
+    final_result = general_analysis(analysis_info)
+    # send the final result to the front end
+    send_result(final_result)
+
+def main():
+    """
+    Examples of how to run this script on shell.
+    """
+    run_cybuy({"apple", "iphone", "email":"vhartoon@usc.edu", "1":"yes", "2":"no", "3":"yes"})
+    # run_cybuy("apple", "ipad")
+    # run_cybuy("apple", "iphone")
+
+
+
+if __name__ == "__main__":
+    main()
